2c5dad0f91740351868f29317d0860c269b6f6a6
Hao Tzun <chaotzun@gmail.com>
2022-10-11 22:25:06 +0800

    card flipping game

---
+++ F74105012_practice_4_2/F74105012_practice_4_2/Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace F74105012_practice_4_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.gameName = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.continueButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.restartButton = new System.Windows.Forms.Button();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.playTab = new System.Windows.Forms.TabPage();
+            this.scoreTab = new System.Windows.Forms.TabPage();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.name = new System.Windows.Forms.Label();
+            this.nameInput = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.scoreboard = new System.Windows.Forms.Label();
+            this.tabControl.SuspendLayout();
+            this.playTab.SuspendLayout();
+            this.scoreTab.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gameName
+            // 
+            this.gameName.AutoSize = true;
+            this.gameName.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gameName.Location = new System.Drawing.Point(12, 9);
+            this.gameName.Name = "gameName";
+            this.gameName.Size = new System.Drawing.Size(117, 25);
+            this.gameName.TabIndex = 0;
+            this.gameName.Text = "翻牌小游戲";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(610, 175);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(86, 31);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "開始游戲";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // continueButton
+            // 
+            this.continueButton.Enabled = false;
+            this.continueButton.Location = new System.Drawing.Point(610, 233);
+            this.continueButton.Name = "continueButton";
+            this.continueButton.Size = new System.Drawing.Size(86, 31);
+            this.continueButton.TabIndex = 2;
+            this.continueButton.Text = "繼續";
+            this.continueButton.UseVisualStyleBackColor = true;
+            this.continueButton.Click += new System.EventHandler(this.continueButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Cursor = System.Windows.Forms.Cursors.Default;
+            this.exitButton.Location = new System.Drawing.Point(610, 409);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(86, 31);
+            this.exitButton.TabIndex = 3;
+            this.exitButton.Text = "離開游戲";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "01.jpg");
+            this.imageList1.Images.SetKeyName(1, "02.jpg");
+            this.imageList1.Images.SetKeyName(2, "03.jpg");
+            this.imageList1.Images.SetKeyName(3, "04.jpg");
+            this.imageList1.Images.SetKeyName(4, "05.jpg");
+            this.imageList1.Images.SetKeyName(5, "06.jpg");
+            this.imageList1.Images.SetKeyName(6, "07.jpg");
+            this.imageList1.Images.SetKeyName(7, "08.jpg");
+            this.imageList1.Images.SetKeyName(8, "card.jpg");
+            // 
+            // restartButton
+            // 
+            this.restartButton.Location = new System.Drawing.Point(610, 287);
+            this.restartButton.Name = "restartButton";
+            this.restartButton.Size = new System.Drawing.Size(86, 31);
+            this.restartButton.TabIndex = 4;
+            this.restartButton.Text = "重新開始";
+            this.restartButton.UseVisualStyleBackColor = true;
+            this.restartButton.Click += new System.EventHandler(this.restartButton_Click);
+            // 
+            // tabControl
+            // 
+            this.tabControl.Controls.Add(this.playTab);
+            this.tabControl.Controls.Add(this.scoreTab);
+            this.tabControl.Location = new System.Drawing.Point(12, 46);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(503, 498);
+            this.tabControl.TabIndex = 5;
+            // 
+            // playTab
+            // 
+            this.playTab.Controls.Add(this.nameInput);
+            this.playTab.Controls.Add(this.name);
+            this.playTab.Controls.Add(this.scoreLabel);
+            this.playTab.Location = new System.Drawing.Point(4, 22);
+            this.playTab.Name = "playTab";
+            this.playTab.Padding = new System.Windows.Forms.Padding(3);
+            this.playTab.Size = new System.Drawing.Size(495, 472);
+            this.playTab.TabIndex = 0;
+            this.playTab.Text = "游玩區";
+            this.playTab.UseVisualStyleBackColor = true;
+            this.playTab.Click += new System.EventHandler(this.playTab_Click);
+            // 
+            // scoreTab
+            // 
+            this.scoreTab.Controls.Add(this.scoreboard);
+            this.scoreTab.Location = new System.Drawing.Point(4, 22);
+            this.scoreTab.Name = "scoreTab";
+            this.scoreTab.Padding = new System.Windows.Forms.Padding(3);
+            this.scoreTab.Size = new System.Drawing.Size(495, 472);
+            this.scoreTab.TabIndex = 1;
+            this.scoreTab.Text = "歷史記錄區";
+            this.scoreTab.UseVisualStyleBackColor = true;
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLabel.Location = new System.Drawing.Point(3, 3);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(84, 20);
+            this.scoreLabel.TabIndex = 0;
+            this.scoreLabel.Text = "分數：100";
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.name.Location = new System.Drawing.Point(108, 3);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(57, 20);
+            this.name.TabIndex = 1;
+            this.name.Text = "名稱：";
+            // 
+            // nameInput
+            // 
+            this.nameInput.Location = new System.Drawing.Point(161, 3);
+            this.nameInput.Name = "nameInput";
+            this.nameInput.Size = new System.Drawing.Size(191, 20);
+            this.nameInput.TabIndex = 2;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // scoreboard
+            // 
+            this.scoreboard.AutoSize = true;
+            this.scoreboard.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreboard.Location = new System.Drawing.Point(12, 13);
+            this.scoreboard.Name = "scoreboard";
+            this.scoreboard.Size = new System.Drawing.Size(0, 20);
+            this.scoreboard.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 566);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.restartButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.continueButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.gameName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl.ResumeLayout(false);
+            this.playTab.ResumeLayout(false);
+            this.playTab.PerformLayout();
+            this.scoreTab.ResumeLayout(false);
+            this.scoreTab.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label gameName;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button continueButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Button restartButton;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage playTab;
+        private System.Windows.Forms.TextBox nameInput;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.TabPage scoreTab;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label scoreboard;
+    }
+}
+
---
+++ F74105012_practice_4_2/F74105012_practice_4_2/Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace F74105012_practice_4_2
+{
+    public partial class Form1 : Form
+    {
+        Button[] buttonArray = new Button[16];
+        int[] randomArray = new int[16];
+        int firstOpened = -1, secondOpened = -1, pairsCorrect = 0;
+        int score = 100;
+        string playerName;
+
+        public void randArrayGen(ref int[] a)
+        {
+            int[] array = new int[16];
+            Random r = new Random();
+            for (int i = 0, j = 0; i < 16; i += 2, j++)
+            {
+                array[i] = j;
+                array[i + 1] = j;
+            }
+
+            a = array.OrderBy(x => r.Next()).ToArray();
+        }
+
+        public void winCheck()
+        {
+            if (pairsCorrect == 8)
+            {
+                DialogResult r = MessageBox.Show("分數：" + score.ToString(), "游戲結束", MessageBoxButtons.RetryCancel, MessageBoxIcon.Information);
+                scoreboard.Text += playerName + "得分為：" + score + "\n";
+                score = 100;
+                pairsCorrect = 0;
+                if (r == DialogResult.Retry)
+                {
+                    for (int i = 0; i < 16; i++)
+                    {
+                        playTab.Controls.Remove(buttonArray[i]);
+                    }
+                    nameInput.Clear();
+                    startButton.Enabled = true;
+                    scoreLabel.Text = "分數：100";
+                }
+            }
+        }
+
+        public void pairCheck(int index)
+        {
+            if (firstOpened == -1)
+            {
+                firstOpened = index;
+            }
+            else if (secondOpened == -1)
+            {
+                if (index == firstOpened)
+                {
+                    return;
+
+                }
+                else secondOpened = index;
+            }
+
+            if (firstOpened != -1 && secondOpened != -1)
+            {
+                if (randomArray[firstOpened] == randomArray[secondOpened])
+                {
+                    buttonArray[firstOpened].Enabled = false;
+                    buttonArray[secondOpened].Enabled = false;
+                    firstOpened = -1;
+                    secondOpened = -1;
+                    pairsCorrect++;
+                    score += 10;
+                    scoreLabel.Text = "分數：" + score.ToString();
+                }
+                else
+                {
+                    continueButton.Enabled = true;
+                    score -= 5;
+                    scoreLabel.Text = "分數：" + score.ToString();
+                    for (int i = 0; i < 16; i++)
+                    {
+                        if (buttonArray[i].Enabled == false)
+                            continue;
+                        if (i == firstOpened || i == secondOpened)
+                            continue;
+
+                        buttonArray[i].Enabled = false;
+                    }
+
+                }
+                winCheck();
+
+            }
+
+        }
+
+        public void buttonClick(object sender, EventArgs e)
+        {
+            Button b = sender as Button;
+            int index = Array.IndexOf(buttonArray, b);
+            b.ImageIndex = randomArray[index];
+            pairCheck(index);
+        }
+
+        public void createBoard()
+        {
+            for (int i = 1, j = 1; i < 17; i++)
+            {
+                buttonArray[i - 1] = new Button();
+                buttonArray[i - 1].SetBounds(75 * ((i - 1) % 4) + 20 * ((i - 1) % 4) + 20, 100 * j - 40, 75, 100);
+                buttonArray[i - 1].ImageList = imageList1;
+                buttonArray[i - 1].ImageIndex = 8;
+                //buttonArray[i - 1].Text = i.ToString();
+                buttonArray[i - 1].Click += buttonClick;
+                //Controls.Add(buttonArray[i - 1]);
+                playTab.Controls.Add(buttonArray[i - 1]);
+
+                if (i % 4 == 0)
+                    j++;
+            }
+
+            randArrayGen(ref randomArray);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void continueButton_Click(object sender, EventArgs e)
+        {
+            buttonArray[firstOpened].ImageIndex = 8;
+            buttonArray[secondOpened].ImageIndex = 8;
+            firstOpened = -1;
+            secondOpened = -1;
+            for (int i = 0; i < 16; i++)
+            {
+                if (buttonArray[i].ImageIndex == 8)
+                    buttonArray[i].Enabled = true;
+            }
+            continueButton.Enabled = false;
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            DialogResult r = MessageBox.Show("確定要離開游戲嗎", "離開游戲", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+            if (r == DialogResult.OK)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void playTab_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void restartButton_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                playTab.Controls.Remove(buttonArray[i]);
+            }
+            nameInput.Clear();
+            startButton.Enabled = true;
+            scoreLabel.Text = "分數：100";
+            score = 100;
+            pairsCorrect = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            /* for(int i = 0; i < 16; i++)
+             {
+                 buttonArray[i].ImageIndex = randomArray[i];
+             } */
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            string temp = nameInput.Text;
+
+            if (temp == "")
+            {
+                MessageBox.Show("名稱不能為空白", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            else if (temp.Length < 3 || temp.Length > 10)
+            {
+                MessageBox.Show("名稱不合格式（>=3 && <=10）", "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            else
+            {
+                playerName = temp;
+                createBoard();
+                startButton.Enabled = false;
+            }
+        }
+    }
+}
